08fff5e8568b4fc558a892fda6ef8bede260b7bd
srishti-parulekar <123884181+srishti-parulekar@users.noreply.github.com>
2024-07-03 12:20:58 +0000

    added jobitems.html

--- app.py
+++ app.py
@@ -2,10 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+  }
+  ]
 
 @app.route('/')
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS, company_name='Jovian')
 
 
 if __name__ == '__main__':
