a43322c43154efeb9040ba41f0e3e24d3793bccc
srishti-parulekar <123884181+srishti-parulekar@users.noreply.github.com>
2024-07-03 12:29:29 +0000

    added requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 
 app = Flask(__name__)
 
@@ -26,6 +26,10 @@ JOBS = [
 def hello_jovian():
   return render_template('home.html',jobs=JOBS, company_name='Jovian')
 
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
